857983d6424326259cf1803837e83854a18ee169
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-11-20 17:41:20 -0600

    Graded Assignment 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import Buttons from "./components/Buttons";
-import Table from "./components/Table";
-
+import Buttons from "./components/Buttons.jsx";
+import Table from "./components/Table.jsx";
+import Banner from "./components/Banner.jsx";
+import { useState } from "react"; 
+import satData from "./components/satData.jsx";
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+       return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+ };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons
+       filterByType={filterByType}
+       setSat={setSat}
+       displaySats={displaySats}/>
+      <Table sat={sat} />
     </div>
   );
 }
